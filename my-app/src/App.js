45b8945d36e8548fa5893cbcb80f58c859af7812
PlasticPath <shailpatel0507@gmail.com>
2023-06-19 23:56:38 +0530

    Added Bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Heading1 from './components/index_heading';
 
 function App() {
   return (
     <div className="App">
-      
+      <Heading1/>
     </div>
   );
 }
